9904b9c13245da8cf85839dda7880d5cdd96bb44
webSugusUVa <168374656+webSugusUVa@users.noreply.github.com>
2024-12-22 15:33:31 +0100

    Updated about.js

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -8,8 +8,8 @@ const About = () => {
             <div className="row justify-content-center">
                 <div className="col-sm-2"></div>
                 <div className="col-lg-8">
-                    <h4>I am a Computer Science graduate from the University of Valladolid, with a passion for undertaking my own projects. 
-                    I have a keen interest in DevSecOps, driven by my love for cybersecurity, coding, and infrastructure. </h4>
+                    <h4>I am a Computer Science graduate from the University of Valladolid, with a C2 level of proficiency in English. 
+                    I have a keen interest in DevSecOps, driven by my love for cybersecurity, coding, and infrastructure, and a passion for undertaking my own projects </h4>
                 </div>
                 <div className="col-sm-2"></div>
             </div>
